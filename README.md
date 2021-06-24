0e3dc778466068576add2376bd7a1115a4f23cb9
hetima <hetima@hetima.com>
2021-06-24 22:56:24 +0900

    1.0.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# StorageLiftControl
+
+Mod for Dyson Sphere Program. Needs BepInEx.
+
+
+If you set the capacity for automation of the target storage to between 1 and 4 (only 1 to 4 slots will be red), carry-ins from sorter will not be transferred to the upper storage.
+
+This mod allows you to separate items by storage stack so they don't get mixed up. Operations such as connecting the lowest storage to an assembler and bringing in materials from each of the upper levels storage will no longer suspend.
+
+Outgoing sorters connected to these storages will work as normal.  
+
+
+ストレージの自動利用容量上限を1から4マスの間に設定（1～4マスだけ赤くする）しておくと、ソーターからの搬入が上段のストレージに転送されなくなります。
+
+この mod を使用することで、積み上げたストレージごとにアイテムを分別でき、混じり合わなくなります。最下段を組立機と接続し、上の段にそれぞれ材料を搬入するような運用が破綻しなくなります。
+
+搬出用ソーターは通常通りに動作します。
+
+
+
+## Release Notes
+
+### v1.0.0
+
+- Initial Release
+
---
+++ StorageLiftControl/StorageLiftControl.cs
@@ -0,0 +1,69 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HTStorageLiftControl
+{
+    [BepInPlugin(__GUID__, __NAME__, "1.0.0")]
+    public class StorageLiftControl : BaseUnityPlugin
+    {
+        public const string __NAME__ = "StorageLiftControl";
+        public const string __GUID__ = "com.hetima.dsp." + __NAME__;
+
+        new internal static ManualLogSource Logger;
+        void Awake()
+        {
+            Logger = base.Logger;
+            //Logger.LogInfo("Awake");
+
+            new Harmony(__GUID__).PatchAll(typeof(Patch));
+        }
+
+
+        public static StorageComponent NextStorageConsiderBans(StorageComponent sc)
+        {
+            if (sc.bans >= 1 && sc.bans <= 4)
+            {
+                return null;
+            }
+
+            return sc.nextStorage;
+        }
+
+        static class Patch
+        {
+
+            [HarmonyTranspiler, HarmonyPatch(typeof(PlanetFactory), "InsertInto")]
+            public static IEnumerable<CodeInstruction> PlanetFactory_InsertInto_Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                //storageComponent = storageComponent.nextStorage;
+                //IL_02fe: ldloc.s storageComponent //そのまま引数に使える
+                //IL_0300: ldfld   class StorageComponent StorageComponent::nextStorage
+                //IL_0305: stloc.s storageComponent
+                FieldInfo f = AccessTools.Field(typeof(StorageComponent), nameof(StorageComponent.nextStorage));
+                MethodInfo m = typeof(StorageLiftControl).GetMethod(nameof(StorageLiftControl.NextStorageConsiderBans));
+
+                foreach (var ins in instructions)
+                {
+                    if (ins.opcode == OpCodes.Ldfld && ins.operand is FieldInfo o && o == f)
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, m);
+                    }
+                    else
+                    {
+                        yield return ins;
+                    }
+                }
+            }
+
+        }
+
+    }
+}
